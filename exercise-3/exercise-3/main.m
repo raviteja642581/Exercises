c4d0a1305ae08508f3408c93b6c1ce1ca9d07607
User1 <chendiraviteja@gmail.com>
2014-05-07 17:07:05 -0400

    exercise-3
    code-3

--- exercise-3/exercise-3/main.m
+++ exercise-3/exercise-3/main.m
@@ -7,27 +7,168 @@
 //
 
 #import <Foundation/Foundation.h>
-@interface Rectangle: NSObject
--(void) setWidth: (int) h;
--(void) setHeight: (int) h;
--(int) width;
--(int) height;
+@interface Ract : NSObject
+
+
+
+// accumulator methods
+
+
+
+-(void) setWidth : (int) w;
+
+-(void) setHeight : (int) h;
+
+-(int) getWidth;
+
+-(int) getHeight;
+
+
+
+
+
+// arithmetic methods
+
+
+
 -(int) area;
+
 -(int) perimeter;
+
+
+
 @end
 
-@imp
+
+
+
+
+
+
+@implementation Ract
+
+{
+    
+    
+    
+    int width, height, area, peri;
+    
+}
+
+
+
+
+
+-(void) setWidth:(int)w
+
+{
+    
+    width = w;
+    
+}
+
+
+
+-(void) setHeight:(int)h
+
+{
+    
+    height = h;
+    
+}
+
+
+
+-(int) getHeight
+
+{
+    
+    return height;
+    
+}
+
+
+
+
+
+-(int) getWidth
+
+{
+    
+    return width;
+    
+}
+
+
+
+
+
+-(int) area
+
+{
+    
+    area= width*height;
+    
+    return area;
+    
+    
+    
+}
+
+
+
+-(int) perimeter
+
+{
+    
+    peri= 2*(width+height);
+    
+    return peri;
+    
+    
+    
+}
+
+@end
+
+
+
 
 
 int main(int argc, const char * argv[])
-{
 
+{
+    
+    
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        
+        
+        Ract *newract = [[Ract alloc] init];
+        
+        [newract setHeight:200];
+        
+        [newract setWidth:100];
+        
+        
+        
+        
+        
+        NSLog(@"\nArea of Ractangle is %i", [newract area]);
+        
+        NSLog(@"\nPerimeter of Ractangle is %i", [newract perimeter]);
+        
+        
+        
+        
         
     }
+    
     return 0;
+    
 }
+
 
